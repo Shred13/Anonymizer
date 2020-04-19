d56cedd301465990b4cc462969f7218cd244527a
shreyanshanand <s.anand@queensu.ca>
2020-04-19 15:35:21 -0400

    is able to anonymize based on what we want

--- anon.js
+++ anon.js
@@ -5,14 +5,13 @@ function handleFiles() {
         let val = getDataExtension(fileList[i]);
         if (val === "json"){
             jsonSet = (readJSON(fileList[i]));
-            console.log(jsonSet);
+
 
         }
         else if (val === "sql") console.log("sql");
         else if (val === "xml") console.log("xml");
         else console.log(val + " file not supported");
     }
-    console.log(set);
 }
 
 function getDataExtension(fileName){
@@ -26,17 +25,19 @@ function readJSON(file) {
         var textFromFileLoaded = JSON.parse(fileLoadedEvent.target.result);
         let table_check;
         let data_array;
+        list_data = [];
         for (var keys in textFromFileLoaded) {
             table_check = textFromFileLoaded[keys];
             if (table_check['type'] === "table") {
                 data_array = table_check['data'];
+                list_data.push(data_array);
                 for (col_names in data_array[0]) {
                     temp.push(col_names)
                 }
             }
         }
-        console.log(temp)
-        print(temp);
+        console.log(list_data);
+        print(temp, list_data);
     //     var myDiv = document.getElementById("cboxes");
     //
     //     for (var i = 0; i < temp.length; i++) {
@@ -56,28 +57,53 @@ function readJSON(file) {
     //     myDiv.append(buttn);
     //
     };
-    console.log(fileReader.readAsText(file));
+    fileReader.readAsText(file);
 
 }
 
-function print(temp){
+function print(temp, data){
         var myDiv = document.getElementById("cboxes");
-
+        temp.sort();
+        sett = new Set(temp);
+        temp = Array.from(sett);
         for (var i = 0; i < temp.length; i++) {
             var checkBox = document.createElement("input");
             var label = document.createElement("label");
             checkBox.type = "checkbox";
             checkBox.value = temp[i];
-            checkBox.id = "id"+i;
+            checkBox.id = "id_checks"+i;
             myDiv.appendChild(checkBox);
             myDiv.appendChild(label);
             label.appendChild(document.createTextNode(temp[i]));
+            myDiv.append(document.createElement("br"))
         }
         var buttn = document.createElement("button");
         buttn.innerHTML = "Submit";
 
         buttn.addEventListener ("click", function(e){
             e.preventDefault();
+            checked_list = [];
+            for (var i = 0; i<temp.length; i++) {
+                if (document.getElementById("id_checks"+i).checked === true){
+                    checked_list.push(document.getElementById("id_checks"+i).value)
+                }
+            }
+            dict_of_table = {};
+            for (j = 0; j<data.length; j++){
+                dict_of_val_in_table = {};
+                for (k = 0; k<data[j].length; k++){
+                    dict_of_few = {};
+                    for (i in checked_list){
+                        if (data[j][k][checked_list[i]]!==undefined){
+                            dict_of_few[checked_list[i]] = data[j][k][checked_list[i]]
+                        }
+                    }
+                    dict_of_val_in_table[k] = dict_of_few;
+                }
+                dict_of_table[j] = dict_of_val_in_table;
+            }
+
+            console.log(dict_of_table);
 
         });
         myDiv.append(buttn);
